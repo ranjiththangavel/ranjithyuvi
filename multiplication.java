777dde5ce7a45d014d1bbeab34c5304e7ff4218d
ranjiththangavel <ranjithyuvi16@gmail.com>
2017-07-07 12:46:13 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=1;i<=10;i++)
+    {
+      System.out.printf("%d * %d =%d\n",n,i,n*i);
+      }
+      }
+      }   
+      
+      
+      
+    
