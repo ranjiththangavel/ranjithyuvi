4c3a3c2665db00f873722f9844aa401a882e1248
ranjiththangavel <ranjithyuvi16@gmail.com>
2017-07-10 13:53:35 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Reverse
+{
+  public static void main(String[] args)
+ {
+     String str="jil jung juck";
+      String[] strArr=str.split("");
+      
+      for(int i=strArr.length-1;i>=0;i--)
+      {
+        System.out.print(str.charAt(i));
+        }
+        }
+        }
+  
